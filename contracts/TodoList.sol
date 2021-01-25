17c9f191206f2ddc5110b899334c647c41b69280
Steve Mackidd <s.mackidd425@mybvc.ca>
2021-01-24 21:59:59 -0700

    Created  a smart contract and started dev on front end. Problem connecting to metamask

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,25 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    //declaring a generic called Task
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    //placing Task in blockchain storage
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Very first task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+
+        //populating the mapped tasks at index taskCount with a new struct Task
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
