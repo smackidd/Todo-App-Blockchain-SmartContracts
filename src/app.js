5acef677615413482d9da0980c0aa6c8d3f0b72a
Steve Mackidd <s.mackidd425@mybvc.ca>
2021-01-28 11:31:25 -0700

    rendered tasks from smart contract to the browser

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 //const web3 = require('../node_modules/web3/lib/index');
 
 App = {
+  loading: false,
+  contracts:  {},
+
   load: async () => {
     await App.loadWeb3()
-    await App.loadAccount() 
+    await App.loadAccount()
+    await App.loadContract() 
+    await App.render()
   },
 
 
@@ -50,6 +55,81 @@ App = {
   loadAccount: async () => {
     App.account = web3.eth.accounts[0]
     console.log(App.account)
+  }, 
+
+  loadContract: async () => {
+    const todoList = await $.getJSON('TodoList.json')
+    //creates a JavaScript version of the smart contract
+    //places todoList.json in a truffle wrapper and points to the address 
+    //on the blockchain where the contract is located.
+    App.contracts.TodoList= TruffleContract(todoList)
+    App.contracts.TodoList.setProvider(App.web3Provider)
+    //console.log(todoList);
+
+    App.todoList = await App.contracts.TodoList.deployed()
+  },
+
+  render: async () => {
+    // Prevent double render
+    if (App.loading) {
+      return
+    }
+
+    // Update app loading state
+    App.setLoading(true)
+
+    //#account references the account id established in HTML
+    $('#account').html(App.account)
+    await App.renderTasks() 
+    
+    App.setLoading(false)
+  },
+
+  setLoading: (boolean) => {
+    App.loading = boolean
+    const loader = $('#loader')
+    const content = $('#content')
+    if (boolean) {
+      loader.show()
+      content.hide()
+    } else {
+      loader.hide()
+      content.show()
+    }
+  }, 
+
+  renderTasks: async() => {
+    // Load the total task count from the blockchain
+    const taskCount = await App.todoList.taskCount()
+    const $taskTemplate = $('.taskTemplate')
+    const $newTaskTemplate = $taskTemplate.clone()
+
+    // Render out each task with a new task template
+    for (var i= 1; i <= taskCount; i++){
+      // Fetch the task data from the blockchain
+      const task = await App.todoList.tasks(i)
+      const taskId = task[0].toNumber()
+      const taskContent = task[1]
+      const taskCompleted = task[2] 
+      
+      // Create the html for the task
+      //$newTaskTemplate = $taskTemplate.clone()
+      $newTaskTemplate.find('.content').html(taskContent)
+      $newTaskTemplate.find('input')
+                      .prop('name', taskId)
+                      .prop('checked', taskCompleted)
+                      //.on('click', App.toggleCompleted)
+
+      // Put the task in the correct list
+      if (taskCompleted) {
+        $('#completedTaskList').append($newTaskTemplate)
+      } else {
+        $('#taskList').append($newTaskTemplate)
+      }
+    }
+
+    // Show the task
+    $newTaskTemplate.show()  
   }
 }
 
